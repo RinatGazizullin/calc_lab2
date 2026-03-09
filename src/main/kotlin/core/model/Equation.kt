c844e35df8fb31ed51b3e2b8218c78aa752da668
Rinat Gazizullin <rinat.gazizullin.31@bk.ru>
2026-03-09 23:22:41 +0300

    fix: fixed diff calculation

--- src/main/kotlin/core/model/Equation.kt
+++ src/main/kotlin/core/model/Equation.kt
@@ -1,11 +1,15 @@
 package core.model
 
+import core.exception.EquationException
 import java.math.BigDecimal
 import java.math.MathContext
 
 class Equation(private val equation: SimpleEquation) {
     companion object {
         private val EPSILON = BigDecimal("1E-6")
+        private val CHECK_DERIVATIVE = BigDecimal("1E-3")
+        private const val DERIVATIVE_ERROR = "Производная не вычисляется в данной точке"
+        private const val DERIVATIVE_DOUBLE_ERROR = "Вторая производная не существует в данной точке"
     }
 
     fun calculate(param: BigDecimal): BigDecimal {
@@ -13,15 +17,32 @@ class Equation(private val equation: SimpleEquation) {
     }
 
     fun derivative(param: BigDecimal): BigDecimal {
+        val left = (equation.apply(param + EPSILON) - equation.apply(param))
+            .divide(EPSILON, MathContext.DECIMAL128)
+        val right = (equation.apply(param) - equation.apply(param - EPSILON))
+            .divide(EPSILON, MathContext.DECIMAL128)
+
+        if ((left - right).abs() > CHECK_DERIVATIVE) {
+            throw EquationException(DERIVATIVE_ERROR)
+        }
+
         return (equation.apply(param + EPSILON) - equation.apply(param - EPSILON))
             .divide(EPSILON * BigDecimal.valueOf(2), MathContext.DECIMAL128)
     }
 
     fun secondDerivative(x: BigDecimal): BigDecimal {
-        val a = derivative(x + EPSILON)
-        val b = derivative(x - EPSILON)
+        val dRight = derivative(x + EPSILON)
+        val dLeft = derivative(x - EPSILON)
+        val dCenter = derivative(x)
 
-        return (a - b) / (EPSILON * BigDecimal.valueOf(2))
+        val diffLeft = (dCenter - dLeft).divide(EPSILON, MathContext.DECIMAL128)
+        val diffRight = (dRight - dCenter).divide(EPSILON, MathContext.DECIMAL128)
+
+        if ((diffLeft - diffRight).abs() > CHECK_DERIVATIVE) {
+            throw EquationException(DERIVATIVE_DOUBLE_ERROR)
+        }
+
+        return (dRight - dLeft).divide(EPSILON * BigDecimal.valueOf(2), MathContext.DECIMAL128)
     }
 
     fun interface SimpleEquation {
