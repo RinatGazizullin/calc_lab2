f093898ec2b142cab77cb9e23798a2b02e340de1
Rinat Gazizullin <rinat.gazizullin.31@bk.ru>
2026-03-10 19:14:50 +0300

    fix: parser fixed for new model data

--- src/main/kotlin/core/parser/EquationParser.kt
+++ src/main/kotlin/core/parser/EquationParser.kt
@@ -2,9 +2,8 @@ package core.parser
 
 import com.ezylang.evalex.Expression
 import com.ezylang.evalex.parser.ParseException
-import com.ezylang.evalex.parser.Token
-import core.exception.EquationException
-import core.model.System
+import core.exception.ExpressionException
+import core.utils.TextUtils
 import java.math.BigDecimal
 
 class EquationParser {
@@ -12,34 +11,19 @@ class EquationParser {
         private const val FORMAT_ERROR = "Неверный формат строки"
     }
 
-    fun parse(equation: String): System.MultiEquation {
+    fun parse(equation: String): core.model.Expression {
         try {
-            val eval = Expression(equation.replace(",", "."))
-            val names = mutableListOf<String>()
-
-            for (node in eval.allASTNodes) {
-                if (node.token.type == Token.TokenType.VARIABLE_OR_CONSTANT &&
-                    !eval.constants.containsKey(node.token.value)) {
-                    val name = node.token.value
-
-                    if (!names.contains(name)) {
-                        names.add(name)
-                    }
-                }
-            }
-
-            val localNames = names.toList()
-            return System.MultiEquation { params: List<BigDecimal> ->
+            val prepared = TextUtils.prepareOnly(equation)
+            val eval = Expression(prepared)
+            val localNames = eval.usedVariables.toSet()
+            return core.model.Expression(localNames.size, localNames, prepared
+            ) { params: Map<String, BigDecimal> ->
                 val expression = eval.copy()
-
-                localNames.forEachIndexed { index, name ->
-                    expression.with(name, params[index])
-                }
-
+                params.keys.forEach { key -> expression.with(key, params[key]) }
                 expression.evaluate().numberValue
             }
         } catch (e: ParseException) {
-            throw EquationException(FORMAT_ERROR)
+            throw ExpressionException(FORMAT_ERROR)
         }
     }
 }
